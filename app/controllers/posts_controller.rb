fbeb08e582d9fd6a63b6411c02deddeb218d85c7
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2018-01-08 12:32:53 -0500

    fixed: in create method i changed post_params to posts.build(post_params)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = current_user(post_params)
+    @post = current_user.posts.build(post_params)
 
     if @post.save
       redirect_to @post
